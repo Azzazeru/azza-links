acdb7cb583d557e79175b1f9b90f7343bbc7a438
Azzazeru <aaron.fuentesdioca@gmail.com>
2026-04-08 15:58:52 -0400

    fix: convert PORT to number for server listening

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,6 @@ import app from './app';
 
 import { PORT } from './config';
 
-app.listen(PORT, () => {
+app.listen(Number(PORT), '0.0.0.0', () => {
   console.log('Server is running on port ' + PORT);
 });
